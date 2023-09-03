3ab4a05c90b40eea2e018085cfb82f9966d4dbca
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-03 13:57:33 +0300

    feat: add toggleNoteEditable btn

--- src/components/NoteActions.tsx
+++ src/components/NoteActions.tsx
@@ -2,20 +2,28 @@ import { memo } from 'react';
 import { Flex, ActionIcon } from '@mantine/core';
 import { IconEdit, IconTrash } from '@tabler/icons-react';
 import notesService from '../services/notesService';
+import { AppState, ActiveNote } from '../context/AppProvider/models';
+import { useAppState } from '../context/AppProvider';
 
 type Props = {
   id: number | undefined;
 };
 
 const NoteActions = memo(function NoteActions({ id }: Props) {
+  const appState: AppState | null = useAppState();
+  const { editable, setEditable } = appState?.activeNote as ActiveNote;
+
   const handleDelete = () => {
-    console.log('DELETE', id);
     if (typeof id === 'number') notesService.delete(id);
   };
-  console.log(id);
+
+  const toggleNoteEditable = () => {
+    setEditable(!editable);
+  };
+
   return (
     <Flex w="2.6rem" justify="space-between" mr={4}>
-      <ActionIcon color="orange">
+      <ActionIcon color="orange" onClick={toggleNoteEditable}>
         <IconEdit size="1rem" stroke={1.5} />
       </ActionIcon>
       <ActionIcon color="red" onClick={handleDelete}>
