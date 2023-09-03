bac9e5677172105dbc24d0bbc8c55bb8a046c597
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-03 14:07:00 +0300

    feat(NotePage): add textEditor

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,3 +1,11 @@
+import { useParams } from 'react-router-dom';
+import { db, Note } from '../db';
+import { useLiveQuery } from 'dexie-react-hooks';
+import TextEditor from '../components/TextEditor';
+
 export default function NotePage() {
-  return <div>NotePage</div>;
+  const { id } = useParams();
+  const note: Note | undefined = useLiveQuery(() => db.notes.get(Number(id)));
+
+  return note && <TextEditor activeNote={note} />;
 }
