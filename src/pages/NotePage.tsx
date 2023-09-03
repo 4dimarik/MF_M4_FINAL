97fb230fbd9e5c6f676a47080d98f337bc785332
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-03 16:04:53 +0300

    fix(Note Page) get note

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -5,7 +5,12 @@ import TextEditor from '../components/TextEditor';
 
 export default function NotePage() {
   const { id } = useParams();
-  const note: Note | undefined = useLiveQuery(() => db.notes.get(Number(id)));
+  const note: Note | undefined = useLiveQuery(
+    () => db.notes.get(Number(id)),
+    [id],
+    undefined
+  );
 
+  console.log('#### NotePage', note);
   return note && <TextEditor activeNote={note} />;
 }
