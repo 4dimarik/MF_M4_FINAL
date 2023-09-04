9a082d4ad9b22c0114d44cc10a38f5f655307329
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-04 19:19:35 +0300

    feat: add filter notes

--- src/components/NoteStack/ui/NoteStack.tsx
+++ src/components/NoteStack/ui/NoteStack.tsx
@@ -6,17 +6,49 @@ import NoteButton from './NoteButton';
 import { useNavigate } from 'react-router-dom';
 import { Note, db } from '../../../db';
 import { useLiveQuery } from 'dexie-react-hooks';
+import {
+  IAppState,
+  ISearch,
+  IActiveNote,
+} from '../../../context/AppProvider/models';
+import { useAppState } from '../../../context/AppProvider';
+import { useEffect } from 'react';
 
 export default function NoteStack() {
   const navigate = useNavigate();
-  const notes: Note[] | undefined = useLiveQuery(() =>
-    db.notes.orderBy('updatedAt').reverse().toArray()
+  const appState: IAppState | null = useAppState();
+  const { value: search } = appState?.search as ISearch;
+  const { setEditable } = appState?.activeNote as IActiveNote;
+
+  const notes: Note[] | undefined = useLiveQuery(
+    () =>
+      search === ''
+        ? db.notes.orderBy('updatedAt').reverse().toArray()
+        : db.notes
+            .orderBy('updatedAt')
+            .reverse()
+            .filter((note) => {
+              const regexp = new RegExp(search, 'ig');
+              return regexp.test(note.content) || regexp.test(note.title);
+            })
+            .toArray(),
+    [search],
+    undefined
   );
 
   const addNewNote = async () => {
     const result: ActionResult = await notesService.add();
-    if (result.status === 'ok') navigate(`/${result.id}`);
+    if (result.status === 'ok') {
+      navigate(`/${result.id}`);
+      setEditable(true);
+    }
   };
+
+  useEffect(() => {
+    if (search !== '' && notes) {
+      navigate(`/${notes[0].id}`);
+    }
+  }, [search, notes]);
 
   return (
     <Navbar width={{ base: 300 }} height="100vh" p="xs">
