e182f59d90aada934525c0a3a9624695624d2e51
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-04 21:08:35 +0300

    feat: add UserMenu

--- src/components/HeaderBlock.tsx
+++ src/components/HeaderBlock.tsx
@@ -1,12 +1,14 @@
 import { memo } from 'react';
 import { Header, Flex } from '@mantine/core';
 import NoteActions from './NoteActions';
+import { UserMenu } from './UserMenu';
 
 const HeaderBlock = memo(function HeaderBlock() {
   return (
     <Header height={60} p="xs">
       <Flex bg="white" justify="end" p="0.5rem">
         <NoteActions />
+        <UserMenu />
       </Flex>
     </Header>
   );
