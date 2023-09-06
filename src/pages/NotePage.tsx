374375f298f8fade3522794dd41d25f65d05d8b8
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-06 20:21:23 +0300

    fix: if note unfefined navigate to 404

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -4,11 +4,22 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import TextEditor from '../components/TextEditor';
 import { IActiveNote } from '../context/AppProvider/models';
 import { useActiveNote } from '../context/AppProvider';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotePage() {
   const { id } = useParams();
+  const navigate = useNavigate();
   let note: Note | undefined = undefined;
-  note = useLiveQuery(() => db.notes.get(Number(id)), [id], undefined);
+  note = useLiveQuery(
+    async () => {
+      const note: Note | undefined = await db.notes.get(Number(id));
+      if (!note) navigate('/404');
+      return note;
+    },
+    [id],
+    undefined
+  );
+
   const activeNote: IActiveNote | undefined = useActiveNote();
   const editable: boolean = activeNote ? activeNote?.editable : false;
 
