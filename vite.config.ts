a9522e793067a83942766c51d5abb71843e55595
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-06 20:01:24 +0300

    feat: config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  outDir: 'dist',
+  manifest: {
+    name: 'React Notepad',
+    short_name: 'ReactNotepad',
+    theme_color: '#1976d2',
+    background_color: '#fafafa',
+    display: 'standalone',
+    start_url: '/',
+    scope: '/',
+    orientation: 'portrait-primary',
+    icons: [
+      {
+        src: 'assets/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vitePWA],
+});
+
---
+++ workbox-config.js
@@ -0,0 +1,5 @@
+export default {
+  globalDirectory: 'dist/',
+  globalPatterns: ['**/*/{css,woff2,png,svg,jpg,jpeg,js}'],
+  swDwst: 'dist/sw.js',
+};
